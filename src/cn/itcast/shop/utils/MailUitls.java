7e1b9c07fe05f08bc22656ac3a0086aeb9842cae
ziyang <972580752@qq.com>
2018-03-07 19:25:45 +0800

    增加用户注册时的邮箱验证

--- src/cn/itcast/shop/utils/MailUitls.java
+++ src/cn/itcast/shop/utils/MailUitls.java
@@ -13,6 +13,8 @@ import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+import sun.net.smtp.SmtpClient;
+
 /**
  * 邮件发送工具类
  * @author 传智.郭嘉
@@ -30,14 +32,35 @@ public class MailUitls {
 		 * 2.创建一个代表邮件的对象Message.
 		 * 3.发送邮件Transport
 		 */
+		
+	//972580752@qq.com向2024644008@qq.com发邮件
+
+		
 		// 1.获得连接对象
 		Properties props = new Properties();
-		props.setProperty("mail.host", "localhost");
+		props.setProperty("mail.smtp.host", "smtp.qq.com");
+		
+		props.setProperty("mail.smtp.auth", "true");//必须 普通客户端
+		
+		props.setProperty("mail.transport.protocol", "smtp");//必须选择协议
+		
+		final String smtpPort = "465";
+        props.setProperty("mail.smtp.port", smtpPort);
+        props.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.setProperty("mail.smtp.socketFactory.fallback", "false");
+        props.setProperty("mail.smtp.socketFactory.port", smtpPort);
+		
+	//	props.setProperty("mail.host", "localhost");
+		
 		Session session = Session.getInstance(props, new Authenticator() {
 
+			//session.setDebug(true);//设置debug模式   在控制台看到交互信息
+			
+			
 			@Override
 			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication("service@shop.com", "111");
+				return new PasswordAuthentication("972580752@qq.com", "picoeygfbloubbgh");
+			//	return new PasswordAuthentication("123@shop.com", "123");
 			}
 			
 		});
@@ -45,14 +68,15 @@ public class MailUitls {
 		Message message = new MimeMessage(session);
 		// 设置发件人:
 		try {
-			message.setFrom(new InternetAddress("service@shop.com"));
+			message.setFrom(new InternetAddress("972580752@qq.com"));
+			//message.setFrom(new InternetAddress("123@shop.com"));
 			// 设置收件人:
 			message.addRecipient(RecipientType.TO, new InternetAddress(to));
 			// 抄送 CC   密送BCC
 			// 设置标题
 			message.setSubject("来自购物天堂传智商城官方激活邮件");
 			// 设置邮件正文:
-			message.setContent("<h1>购物天堂传智商城官方激活邮件!点下面链接完成激活操作!</h1><h3><a href='http://192.168.36.103:8080/shop/user_active.action?code="+code+"'>http://192.168.36.103:8080/shop/user_active.action?code="+code+"</a></h3>", "text/html;charset=UTF-8");
+			message.setContent("<h1>购物天堂传智商城官方激活邮件!点下面链接完成激活操作!</h1><h3><a href='http://192.168.1.1:8080/shop/user_active.action?code="+code+"'>http://192.168.1.1:8080/shop/user_active.action?code="+code+"</a></h3>", "text/html;charset=UTF-8");
 			// 3.发送邮件:
 			Transport.send(message);
 		} catch (AddressException e) {
@@ -64,6 +88,7 @@ public class MailUitls {
 	}
 	
 	public static void main(String[] args) {
-		sendMail("aaa@shop.com","11111111111111");
+		sendMail("2024644008@qq.com","11111111111111");
+	//	sendMail("1@shop.com","11111111111111");
 	}
 }
